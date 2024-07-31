4fbf4f287a0792559d73ad56c9b45ca4fbcfd77c
Calvin Wetzel <joncalvinwetzel22@gmail.com>
2024-07-31 15:37:12 -0400

    Several Models Tried; Not great performance

--- old_src/ReadAndStoreContractorNames.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-with open("contractors.txt", "r") as file:
-    names = file.readlines()
-    names = [name.strip() for name in names]
-
-print(len(names))
-
-np.save("contractor_names.npy", np.array(names))
